6a662e5cd490de8c36071f86f042e11c07ef70a8
praveenkumarj0805 <praveenkumarj0805@gmail.com>
2026-09-16 20:58:21 +0530

    Update RemoveLargestAndProduct.java

--- Basics/RemoveLargestAndProduct.java
+++ Basics/RemoveLargestAndProduct.java
@@ -3,25 +3,6 @@ Problem:
 Repeatedly remove one occurrence of the largest digit
 and find the product of the remaining digits until
 only one digit remains.
-
-Input:
-5824
-
-Output:
-0
-
-Test Cases:
-1. Input: 999
-   Output: 1
-
-2. Input: 12345
-   Output: 2
-
-3. Input: 7777
-   Output: 9
-
-4. Input: 105
-   Output: 0
 */
 
 import java.util.Scanner;
