f90232fd70b00154887f74e4e70311949ba7905d
praveenkumarj0805 <praveenkumarj0805@gmail.com>
2026-09-16 20:24:14 +0530

    Create RemoveLargestAndProduct.java

---
+++ Basics/RemoveLargestAndProduct.java
@@ -0,0 +1,64 @@
+/*
+Problem:
+Repeatedly remove one occurrence of the largest digit
+and find the product of the remaining digits until
+only one digit remains.
+*/
+
+import java.util.*;
+
+public class RemoveLargestAndProduct
+{
+    static int maxDigit(int n){
+        int max=-1;
+        while(n>0){
+            int digit=n%10;
+            if(digit>max){
+                max=digit;
+            }
+            n/=10;
+        }
+        return max;
+    }
+
+    static int remove(int n){
+        int max=maxDigit(n);
+        int result=0;
+        int place=1;
+        boolean flag=false;
+        while(n>0){
+            int digit=n%10;
+            if(digit!=max || flag){
+                result=result+digit*place;
+                place*=10;
+            }
+            else{
+                flag=true;
+            }
+            n/=10;
+        }
+        return result;
+    }
+
+    static int product(int n){
+        if(n==0){
+            return 0;
+        }
+        int p=1;
+        while(n>0){
+            p=p*(n%10);
+            n/=10;
+        }
+        return p;
+    }
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int temp=n;
+        while(temp%10!=temp){
+            temp=product(remove(temp));
+        }
+        System.out.println(temp);
+    }
+}
